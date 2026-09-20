ae9620684d4693dde824ad91ca0324ef7de72ab3
NoobDev <usanumberone97@protonmail.com>
2026-09-20 14:20:27 +0700

    fix: correct link closing tags and streamline inline code parsing

--- converter.py
+++ converter.py
@@ -123,10 +123,7 @@ class MarkdownConverter:
         text = html.escape(text)
 
         # Inline code: `code` - Processed first to avoid interpreting markdown inside code
-        text = re.sub(r'`([^`]*)`', r'<code-block>\1</code-block>', text)
-        
-        # Re-evaluating: the provided source had <code-block> as a temporary marker
-        text = re.sub(r'<code-block>(.*?)</code-block>', r'<code>\1</code>', text)
+        text = re.sub(r'`([^`]*)`', r'<code>\1</code>', text)
         
         # Process bold and italic before links so we can have styling inside links
         # Bold-Italic - Use non-greedy matching
